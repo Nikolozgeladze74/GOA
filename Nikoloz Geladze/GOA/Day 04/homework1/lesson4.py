7c9d5d19eba7e1a54142b129fedbfdc183abc9f1
Nikolozgeladze74 <nikolozgeladze74@yahoo.com>
2024-03-02 00:49:13 +0400

    commit

---
+++ Nikoloz Geladze/GOA/Day 04/homework1/lesson4.py
@@ -0,0 +1,8 @@
+#i started using a code "input"
+#i am going to print the sum of two numbers
+number1 = input("enter a number: ")
+
+number2 = input("enter the second number: ")
+
+print(int(number1) +  int(number2))
+#i have completed the code to print the sum of two numbers
---
+++ Nikoloz Geladze/GOA/Day 04/homework2/lesson4.py
@@ -0,0 +1,13 @@
+#i am going to interview a costumer
+name = input("please enter your name: ")
+
+surname = input("please enter your surname: ")
+
+age = input("please enter your age: ")
+
+question = input("how many years later do you want to time travel?: ")
+#an interview has ended
+print(name,surname,age,question)
+#i am going to print how many year old himself/herself will he/she meet in future
+print("you will meet 35 year old yourself in 20 years")
+#i completed the code of interviewing a costumer
