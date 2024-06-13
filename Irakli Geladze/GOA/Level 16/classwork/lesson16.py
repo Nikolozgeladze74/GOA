7780adc57883fd4abb1d80f676f3da0798cb859b
Nikolozgeladze74 <nikolozgeladze74@yahoo.com>
2024-06-14 00:22:17 +0400

    commit

---
+++ Irakli Geladze/GOA/Level 16/classwork/lesson16.py
@@ -0,0 +1,3 @@
+for GOA in range(50):
+ if GOA % 2 == 1:
+    print(GOA)
---
+++ Irakli Geladze/GOA/Level 16/homework/lesson16.py
@@ -0,0 +1,9 @@
+for i in range(120,501):
+    if i % 3 == 0:
+        print(str(i) + "number divides by 3")
+for i in range(120,501):
+    if i % 5 == 0:
+        print(str(i) + "number divides by 5")       
+for i in range(120,501):
+    if i % 7 == 0:
+        print(str(i) + "number divides by 7")
